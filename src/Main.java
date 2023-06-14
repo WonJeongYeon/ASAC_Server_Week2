27d01d1d2cce1021f96cec5f0b77bf2044ed8e8f
WonJeongYeon <wonjungyoun@naver.com>
2023-06-14 15:51:50 +0900

    feat: game base logic define

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        InputView iv = new InputView();
+        NumberGenerator n = new RandomNumberGenerator();
+        int[] answer = n.numbers();
+        System.out.println(Arrays.toString(answer));
+
+        boolean gameFlag = false;
+        while (!gameFlag) {
+
+            int[] input = iv.userInput();
+            System.out.println(Arrays.toString(input));
+            if (Arrays.equals(answer, input)) {
+                gameFlag = true;
+            }
+        }
+
+        System.out.println("게임 종료");
+
     }
 }
